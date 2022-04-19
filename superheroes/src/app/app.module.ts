7b36ae18283e8f5b4729d6925596c4ca5cb9ab1b
Sibyl Pérez <51963634+sibylperez@users.noreply.github.com>
2022-04-19 11:31:35 -0300

    first commit

---
+++ superheroes/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ErrorViewComponent } from './shared/error-view/error-view.component';
+import { OtherComponent } from './shared/other/other.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ErrorViewComponent,
+    OtherComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
